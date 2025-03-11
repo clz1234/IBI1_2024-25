d9f9c352e154783140e6fec8c50dd557d96045b8
纸彩鸢虹 <cuilizi@zhicaiyuanhongdeMacBook-Pro.local>
2025-03-11 11:09:50 +0800

    truth table

---
+++ Prectical4/booleans.py
@@ -0,0 +1,11 @@
+X = True
+Y = False
+W = X and Y 
+print(W)
+# The truth table for W
+# | X | Y | W（X and Y） |
+# |----|----|----|
+# | True | True | True |
+# | True | False | False |
+# | False | True | False |
+# | False | False | False |
