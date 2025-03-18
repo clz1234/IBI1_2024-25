a4fa161e11d9441c8fb8f970b77876f414042539
纸彩鸢虹 <cuilizi@zhicaiyuanhongdeMacBook-Pro.local>
2025-03-18 12:40:58 +0800

    select

--- Practical5/language.py
+++ Practical5/language.py
@@ -1,4 +1,8 @@
-language_users = { "JavaScript": 62.3, "HTML": 52.9, "Python":51, "SQL": 51, "TypeScript": 38.5 }
+language_users = { "JavaScript": 62.3,
+                   "HTML": 52.9,
+                   "Python":51,
+                   "SQL": 51,
+                   "TypeScript": 38.5 }
 # Create an dictionary. key is "language", value is "users"
 print(language_users)
 # Print this dictionary. 
@@ -13,10 +17,20 @@ plt.title("Programming languages among developers worldwide as of 2024", fontsiz
 plt.ylim(0,80)
 # Set the display range of the Y-axis
 plt.ylabel("Usage Percentage ", fontsize = 14)
-# Set the Y-axis label, specifying a font size of 16 for the Y-axis label.
+# Set the Y-axis label, specifying a font size of 14 for the Y-axis label.
 plt.xlabel("Programming Languages", fontsize = 14)
-# Set the X-axis label, specifying a font size of 16 for the X-axis label.
+# Set the X-axis label, specifying a font size of 14 for the X-axis label.
 plt.yticks([20, 40, 60, 80])
 # Set the scale of the Y-axis
 plt.show()
 # Show the bar chart
+
+input_language = input("input the programming language")
+# User input programming language
+if input_language in language_users:
+# Check that the language you entered is in the dictionary
+    print(f"The percentage of {input_language}is {language_users[input_language]}%")
+    # If in output "The	percentage of developers	who	use	one	language taken from	the	input list."
+else:
+    print(f"{input_language} is not in the dictionary")
+    # If not output "input_language is not in dictionary"
