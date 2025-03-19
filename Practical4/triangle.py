b11e509cd03b39abd3e005090ae2bb7cd681db16
纸彩鸢虹 <cuilizi@zhicaiyuanhongdeMacBook-Pro.local>
2025-03-19 13:09:38 +0800

    change

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -3,17 +3,11 @@
 #            It can be calculated as the sum of the n numbers from 1 to n
 #            for n in range (1, 11)
 
-# Compute the first ten values of the triangular series
-# Step 1: Initialize a variable to store the sum of the sequence
-# Step 2: Use a loop from 1 to 10
-# Step 3: In each loop, count the number of triangles for the current number
-# Step 4: Print out the number of triangles
-
-# Step 1: Initialize a variable to store the sum of the sequence
+# initialize a variable to store the sum of the sequence
 y = 0
-# Step 2: Use a loop from 1 to 10
+# use a loop from 1 to 10
 for n in range(1, 11):
-# Step 3: In each loop, count the number of triangles for the current number
+# in each loop, count the number of triangles for the current number
     y += n
-# Step 4: Print out the number of triangles
-    print(n,y)
+# print out the number of triangles
+    print(n,y
