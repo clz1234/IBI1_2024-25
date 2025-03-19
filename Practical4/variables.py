523b41aa0fe6f7fb018f42417bbbf2dcda562c77
纸彩鸢虹 <cuilizi@zhicaiyuanhongdeMacBook-Pro.local>
2025-03-19 13:13:11 +0800

    change

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,15 +1,15 @@
 a = 15 
-# The walk to the bus stop is 15 mins
+# the walk to the bus stop is 15 mins
 b = 75
-# The bus journey takes 75 mins 
+# the bus journey takes 75 mins 
 c = a+b
-# The total length of time for the bus-based commute
+# the total length of time for the bus-based commute
 d = 90
-# The drive takes 90 mins
+# the drive takes 90 mins
 e = 5
-# The walk from the car park takes 5 mins
+# the walk from the car park takes 5 mins
 f = d+e
-# The total length of time for the car-based commute
+# the total length of time for the car-based commute
 print(c , f)
 if c > f:
     print("Bus-based commute is longer, car-based commute faster than bus-based")
@@ -17,30 +17,30 @@ elif c < f:
     print("Car-based commute is longer, bus-based commute faster than car-based")
 else:
     print("car-based commute is same as bus-based")
-    # Car-based commute is longer,bus-based commute is quicker
+    # car-based commute is longer,bus-based commute is quicker
 
 X = True
 Y = False
 W = X and Y
 print(W)
-# The result is False
+# the result is False
 X = True
 Y = True
 W = X and Y
 print(W)
-# The result is True
+# the result is True
 X = False
 Y = False
 W = X and Y
 print(W)
-# The result is False
+# the result is False
 X = False
 Y = True
 W = X and Y
-print(W)
-# The result is False
+print(W
+# the result is False
 
-# The truth table for W
+# the truth table for W
 # | X | Y | W（X and Y） |
 # |----|----|----|
 # | True | True | True |
