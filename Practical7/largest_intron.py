2276289ccfa48000f0953363dd0cf7c6b327b54b
纸彩鸢虹 <lizi.24@intl.zju.edu.cn>
2025-04-02 16:44:44 +0800

    add some comments

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -4,11 +4,15 @@
 #set the largest_intron's first element in largest_intron_length to get the intron's length
 # print the intron's sequence
 # print the length 
+
+#import library
 import re
 #create a string variable seq
 sequence = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 #I use the greedy matching to find the largest intron
 largest_intron = re.findall(r'GT.+AG', sequence)
+#to get the length
 largest_intron_length = largest_intron[0]
+#print result
 print(largest_intron)
 print(len(largest_intron_length))
