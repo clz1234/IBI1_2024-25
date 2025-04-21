85d098ee762f9ea7833c009f2bdc3d62984db6b2
纸彩鸢虹 <lizi.24@intl.zju.edu.cn>
2025-04-21 20:31:11 +0800

    add comment

--- Practical8/drug.py
+++ Practical8/drug.py
@@ -1,33 +1,31 @@
 #define a function drug_dosage_calculator, includes two parameters, weight and strength
 #check that the weight and strength are within the specified ranges
+#use ValueError to output error
 #calculate the recommended dose of the drug by multiplying your body weight by 15.
 #the volume is calculated based on the different strengths
-#input the weight and strength
+#input the weight and strength as an example to call my code
 #check the function, if it work, print the result, else print the error information
 
-#define the function
 def drug_dosage_calculator(weight, strength):
     #convert a string to a floating point number
     weight = float(weight)
     #check the weight and strength
     if not 10 <= weight <= 100:
-        raise ValueError
-    if strength not in ["120 mg/5 ml", "250 mg/5 ml"]:
-        raise ValueError
+        raise ValueError("the weigth must between 10 and 100")
+    if strength not in ["120mg/5ml", "250mg/5ml"]:
+        raise ValueError("the strength must be 120mg/5ml or 250mg/5ml")
     #calculate the recommended dose with different strength
     recommended_dose = 15 * weight
-    if strength == "120 mg/5 ml":
+    if strength == "120mg/5ml":
         volume = recommended_dose / 120 * 5
     else:
         volume = recommended_dose / 250 * 5
     #stop the function
     return volume
 #input the weigth and strength
-weight = input("the patient's weight")
-strength = input("the strength of the drug(120mg/5ml or 250mg/5ml)")
+weight ="66"
+strength ="120mg/5ml"
 #check the function, if it work, print the result, else print the error
-try:
-    result = drug_dosage_calculator(weight, strength)
-    print(f"the drug required is {result}ml")
-except ValueError as e:
-    print(f"error: {e}")
+result = drug_dosage_calculator(weight, strength)
+if result is not None:
+    print(f"The drug required is {result} ml.")
